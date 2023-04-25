07c689677c547f15da3c082821d1062ee19f8b1c
pmajdalani <90423821+pmajdalani@users.noreply.github.com>
2023-04-24 22:56:36 -0400

    commented main

--- main.cpp
+++ main.cpp
@@ -29,18 +29,23 @@ std::vector<std::vector<double>> file_to_vector(std::string fname){
 int main(int argc, char*argv[]) {
     std::string fname = argv[1];
 
-    int node, n; //this might need to be a double? n is for the initial state matrix * P^n
+    int node, n;
 
+    // Initialize vectors
     std::vector<std::vector<double>> initial_vector,pre_transition_vector;
 
     pre_transition_vector = file_to_vector(fname);
+    
+    // Initialize transition matrix 
     SparseMatrix transition = SparseMatrix(pre_transition_vector);
     
+    // Do while loop for user input that gets starting node, only accepts integer values
     do{
     std::cout << "Please enter which node to start at: ";
     std::cin >> node;
     }while(isdigit(node));
-
+    
+    // Checks if starting node is within range of the transition matrix
     if(node > transition.getNumCols() || node > transition.getNumRows()){
         std::cout << "Starting point is out of range" << "\n"
         "Please enter a integer between " << transition.getNumRows() << " and " << transition.getNumCols() << std::endl;
@@ -48,12 +53,13 @@ int main(int argc, char*argv[]) {
         std::cin >> node;
     }
     
-
+    // Do while loop that gets user input for the amount of iterations, only accepts integer values
     do{
     std::cout << "Please enter the amount of iterations: ";
     std::cin >> n;
     }while(isdigit(n));
-
+    
+    // For loop to fill the intiial vector
     initial_vector.push_back({});
     for(int i = 1; i <= transition.getNumRows(); i++){
         if(i != node){
@@ -63,15 +69,17 @@ int main(int argc, char*argv[]) {
             initial_vector[0].push_back(1);
         }
     }
-
+    
+    // Turns the initial vector into a sparse matrix
     SparseMatrix initial_matrix = SparseMatrix(initial_vector);
-
+    
+    // Calculates the P^n, needed to find X sub n.
     SparseMatrix partial = transition^n;
 
    
-
+    // Equation where X sub n = intial matrix * P^n
     SparseMatrix state_matrix = (initial_matrix * partial);
 
-    std::cout << state_matrix.getHead() << std::endl;
+    //Output
     state_matrix.print();
 }
